ab6bccc5afe7f222028f47869db60e47c6442ef0
Alex Charlton <alex.n.charlton@gmail.com>
2014-12-10 11:03:04 -0500

    Important files missing from last commit

---
+++ hypergiant-render-pipeline.scm
@@ -0,0 +1,175 @@
+(module hypergiant-render-pipeline
+  (export-pipeline
+   define-pipeline
+   define-alpha-pipeline
+   dynamic-pipeline
+   dynamic-alpha-pipeline
+   add-node
+   make-render-pipeline)
+
+(import chicken scheme foreign)
+(use (prefix hyperscene scene:) (prefix glls-render glls:) gl-utils
+     srfi-69 lolevel srfi-1 srfi-99)
+(import-for-syntax (prefix glls-render glls:) (prefix hyperscene scene:) srfi-99)
+
+(define-record-type render-pipeline
+  #t #t
+  (dynamic?) (shader) (scene) (scene-arrays) (make-renderable) (render-fun) (render-arrays-fun))
+
+(define renderable-table (make-hash-table))
+
+(define-external (dynamicRender (c-pointer renderable)) void
+  ((hash-table-ref renderable-table renderable) renderable))
+
+(define-external (dynamicPreRender (c-pointer renderable)) void
+  #f)
+
+(define-external (dynamicPostRender) void
+  #f)
+
+(define dynamic-pipeline (scene:add-pipeline #$dynamicPreRender
+                                             #$dynamicRender
+                                             #$dynamicPostRender
+                                             #f))
+
+(define dynamic-alpha-pipeline (scene:add-pipeline #$dynamicPreRender
+                                                   #$dynamicRender
+                                                   #$dynamicPostRender
+                                                   #t))
+
+(begin-for-syntax
+ (define (make-render-pipeline) #f)
+ (define (dynamic-pipeline) #f)
+ (define (dynamic-alpha-pipeline) #f))
+
+(define-syntax define-pipeline
+  (ir-macro-transformer
+   (lambda (exp i c)
+     (let* ((name (strip-syntax (cadr exp)))
+            (pipeline-name (symbol-append name '-render-pipeline))
+            (fast-draw-funs (symbol-append name '-fast-render-functions))
+            (draw-fun (symbol-append 'render- name))
+            (draw-arrays-fun (symbol-append 'render-arrays- name))
+            (renderable-maker (symbol-append 'make- name '-renderable)))
+       `(begin
+          (glls:define-pipeline ,@(cdr exp))
+          ,(if (feature? compiling:)
+               `(define ,pipeline-name
+                  (let-values (((_ __ ___ ____ begin render end render-arrays)
+                                (,fast-draw-funs)))
+                    (make-render-pipeline
+                     #f ,name
+                     (set-finalizer! (scene:add-pipeline begin render end #f)
+                                     scene:delete-pipeline)
+                     (set-finalizer! (scene:add-pipeline begin render-arrays end #f)
+                                     scene:delete-pipeline)
+                     ,renderable-maker
+                     #f #f)))
+               `(define ,pipeline-name
+                  (make-render-pipeline
+                   #t ,name
+                   dynamic-pipeline
+                   #f
+                   ,renderable-maker
+                   ,draw-fun
+                   ,draw-arrays-fun))))))))
+
+(define-syntax define-alpha-pipeline
+  (ir-macro-transformer
+   (lambda (exp i c)
+     (let* ((name (strip-syntax (cadr exp)))
+            (pipeline-name (symbol-append name '-render-pipeline))
+            (fast-draw-funs (symbol-append name '-fast-render-functions))
+            (draw-fun (symbol-append 'render- name))
+            (draw-arrays-fun (symbol-append 'render-arrays- name))
+            (renderable-maker (symbol-append 'make- name '-renderable)))
+       `(begin
+          (glls:define-pipeline ,@(cdr exp))
+          ,(if (feature? compiling:)
+               `(define ,pipeline-name
+                  (let-values (((_ __ ___ ____ begin render end render-arrays)
+                                (,fast-draw-funs)))
+                    (make-render-pipeline
+                     #f ,name
+                     (set-finalizer! (scene:add-pipeline begin render end #t)
+                                     scene:delete-pipeline)
+                     (set-finalizer! (scene:add-pipeline begin render-arrays end #t)
+                                     scene:delete-pipeline)
+                     ,renderable-maker
+                     #f #f)))
+               `(define ,pipeline-name
+                  (make-render-pipeline
+                   #t ,name
+                   dynamic-alpha-pipeline
+                   #f
+                   ,renderable-maker
+                   ,draw-fun
+                   ,draw-arrays-fun))))))))
+
+(define-syntax export-pipeline
+  (ir-macro-transformer
+   (lambda (expr i c)
+     (cons 'export
+           (flatten
+            (let loop ((pipelines (cdr expr)))
+              (if (null? pipelines)
+                  '()
+                  (if (not (symbol? (car pipelines)))
+                      (syntax-error 'export-shader "Expected a pipeline name" expr)
+                      (cons (let* ((name (strip-syntax (car pipelines)))
+                                   (render (symbol-append 'render- name))
+                                   (make-renderable (symbol-append 'make- name
+                                                                   '-renderable))
+                                   (fast-funs (symbol-append name
+                                                             '-fast-render-functions))
+                                   (render-pipeline (symbol-append name
+                                                                   '-render-pipeline)))
+                              (list name render make-renderable fast-funs
+                                    render-pipeline))
+                            (loop (cdr pipelines)))))))))))
+
+(define current-vars (list mvp: (scene:current-camera-model-view-projection)
+                           view: (scene:current-camera-view)
+                           projection: (scene:current-camera-projection)
+                           view-projection: (scene:current-camera-view-projection)
+                           camera-position: (scene:current-camera-position)
+                           inverse-transpose-model: (scene:current-inverse-transpose-model)
+                           n-lights: (scene:n-current-lights)
+                           light-positions: (scene:current-light-positions)
+                           light-colors: (scene:current-light-colors)
+                           light-intensities: (scene:current-light-intensities)
+                           light-directions: (scene:current-light-directions)
+                           ambient: (scene:current-ambient-light)))
+
+;; TODO should add-node always alocate data?
+;; Is being able to reuse a renderable worth it?
+(define (add-node parent pipeline . args)
+  (let* ((glls-pipeline (render-pipeline-shader pipeline))
+         (make-renderable (render-pipeline-make-renderable pipeline))
+         (data (allocate (glls:renderable-size glls-pipeline)))
+         (mesh (get-keyword mesh: args))
+         (usage (get-keyword usage: args (lambda () #:static)))
+         (draw-arrays? (or (get-keyword draw-arrays: args)
+                           (not (and mesh (mesh-index-data mesh)))))
+         (dynamic? (render-pipeline-dynamic? pipeline))
+         (hps-pipeline (if (or dynamic? (not draw-arrays?))
+                           (render-pipeline-scene pipeline)
+                           (render-pipeline-scene-arrays pipeline)))
+         (node (scene:add-node parent
+                               data 
+                               hps-pipeline
+                               (foreign-value "&free" c-pointer))))
+    (when mesh
+      (unless (mesh-vao mesh)
+        (mesh-make-vao! mesh (glls:pipeline-mesh-attributes glls-pipeline) usage)))
+    (apply make-renderable data: data (append args
+                                              (list model: (scene:node-transform node))
+                                              current-vars))
+    (when dynamic?
+      (hash-table-set! renderable-table data
+                       (if draw-arrays?
+                           (render-pipeline-render-arrays-fun pipeline)
+                           (render-pipeline-render-fun pipeline))))
+    node))
+
+) ; end module hypergiant-render-pipeline
---
+++ input.scm
@@ -0,0 +1,195 @@
+(export make-bindings
+        make-binding
+        binding?
+        binding-id
+        add-binding
+        get-binding
+        remove-binding
+        change-binding
+        key-bindings
+        push-key-bindings
+        pop-key-bindings
+        char-callback
+        mouse-bindings
+        push-mouse-bindings
+        pop-mouse-bindings
+        cursor-movement-callback
+        scroll-callback
+        get-cursor-position
+        set-cursor-position
+        get-cursor-world-position)
+
+(define-record-type binding
+  %make-binding
+  #t
+  (id) (key) (scancode?) (mods) (press) (release) (pressed?))
+
+(define-record-printer (binding b out)
+  (fprintf out "#<binding ~S>"
+    (binding-id b)))
+
+(define (make-binding id key #!key scancode? mods (press (lambda () #f)) (release (lambda () #f))
+                      toggle reverse-toggle)
+    (let ((mods (if mods
+                    (apply bitwise-ior mods)
+                    0)))
+      (cond
+       (toggle
+        (%make-binding id key scancode? mods
+                      (lambda () (toggle (add1 (toggle))))
+                      (lambda () (toggle (sub1 (toggle))))
+                      #f))
+       (reverse-toggle
+        (%make-binding id key scancode? mods
+                      (lambda () (reverse-toggle (sub1 (reverse-toggle))))
+                      (lambda () (reverse-toggle (add1 (reverse-toggle))))
+                      #f))
+       (else (%make-binding id key scancode? mods press release #f)))))
+
+(define (make-bindings bindings)
+  (map (lambda (b) (apply make-binding b)) bindings))
+
+(define (add-binding bindings binding)
+  (cons (apply make-binding binding)
+        bindings))
+
+(define (get-binding bindings id)
+  (find (lambda (b) (equal? (binding-id b) id))
+        bindings))
+
+(define (remove-binding bindings id)
+  (remove (lambda (b) (equal? (binding-id b) id))
+          bindings))
+
+(define (change-binding bindings id binding)
+  (if* (find (lambda (b) (equal? (binding-id b) id))
+             bindings)
+       (cons (apply make-binding binding)
+             (delete it bindings))
+       (error 'set-binding! "Not a binding id:" id)))
+
+;;; Keyboard
+(define key-bindings (make-parameter '()))
+
+(define (push-key-bindings bindings)
+  (cond
+   ((list? bindings)
+    (key-callback key-binding-event))
+   (else (key-callback bindings)))
+  (key-bindings (cons bindings
+                      (key-bindings))))
+
+(define (pop-key-bindings)
+  (unless (null? (key-bindings))
+    (key-bindings (cdr (key-bindings)))
+    (unless (null? (key-bindings))
+      (cond
+       ((list? (car (key-bindings)))
+        (key-callback key-binding-event))
+       (else (key-callback (car (key-bindings))))))))
+
+(define (key-binding-event key scancode action mods)
+  (when (not (null? (key-bindings)))
+    (let ((bindings (car (key-bindings))))
+      (cond
+       ((= action %+press+)
+        (let loop ((bindings bindings))
+          (unless (null? bindings)
+            (let ((binding (car bindings)))
+              (if (and (if (binding-scancode? binding)
+                         (= (binding-key binding) scancode)
+                         (= (binding-key binding) key)) 
+                     (= (binding-mods binding) mods))
+                  (begin ((binding-press binding))
+                         (binding-pressed?-set! binding #t))
+                  (loop (cdr bindings)))))))
+       ;; Release for all bindings that share the same key, even with different mods
+       ((= action %+release+)
+        (for-each (lambda (binding)
+                    (when (and (if (binding-scancode? binding)
+                                 (= (binding-key binding) scancode)
+                                 (= (binding-key binding) key))
+                             (binding-pressed? binding))
+                      ((binding-release binding))
+                      (binding-pressed?-set! binding #f)))
+                  bindings))))))
+
+(define key-callback (make-parameter (lambda (key scancode action mods) #f)))
+(define char-callback (make-parameter (lambda (char) #f)))
+
+(define-external (hpgKeyCallback (c-pointer window) (int key) (int scancode)
+                                 (int action) (int mods))
+    void
+  ((key-callback) key scancode action mods))
+
+(define-external (hpgCharCallback (c-pointer window) (unsigned-int char))
+  void
+  ((char-callback) char))
+
+
+;;; Mouse
+(define mouse-bindings (make-parameter '()))
+
+(define (push-mouse-bindings bindings)
+  (mouse-bindings (cons bindings
+                       (mouse-bindings))))
+
+(define (pop-mouse-bindings)
+  (unless (null? (mouse-bindings))
+    (mouse-bindings (cdr (mouse-bindings)))))
+
+(define (mouse-click window button action mods)
+  (when (not (null? (key-bindings)))
+    (let ((bindings (car (mouse-bindings))))
+      (cond
+       ((= action %+press+)
+        (let loop ((bindings bindings))
+          (unless (null? bindings)
+            (let ((binding (car bindings)))
+              (if (and (= (binding-key binding) button) 
+                     (= (binding-mods binding) mods))
+                  (begin ((binding-press binding))
+                         (binding-pressed?-set! binding #t))
+                  (loop (cdr bindings)))))))
+       ;; Release for all bindings that share the same button, even with different mods
+       ((= action %+release+)
+        (for-each (lambda (binding)
+                    (when (and (= (binding-key binding) button)
+                             (binding-pressed? binding))
+                      ((binding-release binding))
+                      (binding-pressed?-set! binding #f)))
+                  bindings))))))
+
+(define cursor-movement-callback (make-parameter (lambda (x y) #f)))
+(define scroll-callback (make-parameter (lambda (x y) #f)))
+
+(define-external (hpgCursorPositionCallback (c-pointer window)
+                                            (double x) (double y))
+    void
+  ((cursor-movement-callback) x y))
+
+
+(define-external (hpgScrollCallback (c-pointer window)
+                                    (double x) (double y))
+    void
+  ((scroll-callback) x y))
+
+(define (get-cursor-position)
+  (%get-cursor-position (%window)))
+
+(define (set-cursor-position x y)
+  (%set-cursor-position (%window) x y))
+
+(define (get-cursor-world-position)
+  (define (scale x) (sub1 (* x 2)))
+  (let-values (((w h) (get-window-size))
+               ((x y) (get-cursor-position)))
+    (let* ((ivp (make-f32vector 16))
+           (x (scale (/ x w)))
+           (y (scale (- 1 (/ y h))))
+           (near (make-point x y -1))
+           (far (make-point x y 1)))
+      (inverse (scene:current-camera-view-projection) (->pointer ivp))
+      (m*vector! ivp near)
+      (m*vector! ivp far)
+      (values near far))))
