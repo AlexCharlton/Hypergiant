e5695b440ddc006dfdd141dc1f7052c63f108a3b
Alex Charlton <alex.n.charlton@gmail.com>
2014-12-17 15:34:44 -0500

    Shaders should all take vec3 positions

--- shaders.scm
+++ shaders.scm
@@ -34,11 +34,11 @@
      (set! gl:frag-color (vec4 c 1.0)))))
 
 (define-pipeline texture-pipeline
-  ((#:vertex input: ((position #:vec2) (tex-coord #:vec2))
+  ((#:vertex input: ((position #:vec3) (tex-coord #:vec2))
              uniform: ((mvp #:mat4))
              output: ((tex-c #:vec2)))
    (define (main) #:void
-     (set! gl:position (* mvp (vec4 position 0.0 1.0)))
+     (set! gl:position (* mvp (vec4 position 1.0)))
      (set! tex-c tex-coord)))
   ((#:fragment input: ((tex-c #:vec2))
                uniform: ((tex #:sampler-2d)))
@@ -46,11 +46,11 @@
      (set! gl:frag-color (texture-2d tex tex-c)))))
 
 (define-alpha-pipeline sprite-pipeline
-  ((#:vertex input: ((position #:vec2) (tex-coord #:vec2))
+  ((#:vertex input: ((position #:vec3) (tex-coord #:vec2))
              uniform: ((mvp #:mat4))
              output: ((tex-c #:vec2)))
    (define (main) #:void
-     (set! gl:position (* mvp (vec4 position 0.0 1.0)))
+     (set! gl:position (* mvp (vec4 position 1.0)))
      (set! tex-c tex-coord)))
   ((#:fragment input: ((tex-c #:vec2))
                uniform: ((tex #:sampler-2d)))
@@ -58,11 +58,11 @@
      (set! gl:frag-color (texture-2d tex tex-c)))))
 
 (define-pipeline text-pipeline
-  ((#:vertex input: ((position #:vec2) (tex-coord #:vec2))
+  ((#:vertex input: ((position #:vec3) (tex-coord #:vec2))
              uniform: ((mvp #:mat4))
              output: ((tex-c #:vec2))) 
    (define (main) #:void
-     (set! gl:position (* mvp (vec4 position 0.0 1.0)))
+     (set! gl:position (* mvp (vec4 position 1.0)))
      (set! tex-c tex-coord)))
   ((#:fragment input: ((tex-c #:vec2))
                uniform: ((tex #:sampler-2d)
