627f64f90e3a6514e9a71ee697c7c8083bdb8ce0
Alex Charlton <acharlton@foko.co>
2016-04-24 20:54:21 -0400

    Add normal-pipeline

--- pipelines.scm
+++ pipelines.scm
@@ -2,6 +2,8 @@
 
 ;;;; Standard GL pipelines, conditionally imported by shaders.scm
 
+(export-pipeline normal-pipeline)
+
 (define-pipeline mesh-pipeline
   ((#:vertex input: ((position #:vec3))
              uniform: ((mvp #:mat4)))
@@ -64,3 +66,30 @@
    (define (main) #:void
      (let ((r #:float (field (texture tex tex-c) r)))
        (set! frag-color (vec4 color r))))))
+
+(define-pipeline normal-pipeline
+  ((#:vertex input: ((position #:vec3) (normal #:vec3))
+             output: ((vnormal #:vec3)))
+   (define (main) #:void
+     (set! gl:position (vec4 position 1.0))
+     (set! vnormal normal)))
+  ((#:geometry input: ((vnormal (#:array vec3)))
+               uniform: ((mvp #:mat4))
+               output: ((color #:vec3))
+               prelude: "layout(triangles) in;\n\
+layout(line_strip, max_vertices = 2) out;\n")
+   (define (main) #:void
+     (let ((n #:vec3 (array-ref vnormal 0))
+           (v0 #:vec4 (field (array-ref gl_in 0) gl:Position))
+           (v1 #:vec4 (+ v0 (vec4 (normalize n) 0))))
+       (set! color (abs n))
+       (set! gl:Position (* mvp v0))
+       (EmitVertex)
+       (set! gl:Position (* mvp v1))
+       (EmitVertex)
+       (EndPrimitive))))
+  ((#:fragment input: ((color #:vec3))
+               output: ((frag-color #:vec4)))
+   (define (main) #:void
+     (set! frag-color (vec4 color 1)))))
+
